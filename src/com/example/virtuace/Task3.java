d0ab8549944e5d675ee41c5ac2dda7c9dc0b6eee
Scav <giorgi.gedevanidze.1@gmail.com>
2021-05-15 18:47:43 +0400

    Task 3 initial commit

---
+++ src/com/example/virtuace/Task3.java
@@ -0,0 +1,22 @@
+package com.example.virtuace;
+
+public class Task3 {
+    public static void main(String[] args) {
+        showStopLightColor(60.1F);
+    }
+
+    //shows the stoplight's expected color after an hour + input minutes
+    public static void showStopLightColor(float minutes) {
+        String green = "green"; // 3 min
+        String yellow = "yellow"; // 1 min
+        String red = "red"; // 1 min
+
+        if(minutes % 3 > 0 && minutes % 4 > 0 && minutes % 4 < 3 && minutes % 4 >= minutes % 5) {
+            System.out.println(green);
+        } else if (minutes % 3 >= 0 && minutes % 2 >= 1){
+            System.out.println(yellow);
+        } else {
+            System.out.println(red);
+        }
+    }
+}
