961bb27f5d727f1078053697149f08ae77c6c3e4
Scav <giorgi.gedevanidze.1@gmail.com>
2021-05-17 19:57:40 +0400

    Made minor adjustments in the algorithm

--- src/com/example/virtuace/Task3.java
+++ src/com/example/virtuace/Task3.java
@@ -2,7 +2,7 @@ package com.example.virtuace;
 
 public class Task3 {
     public static void main(String[] args) {
-        showStopLightColor(60.1F);
+        showStopLightColor(5.0001F * 12);
     }
 
     //shows the stoplight's expected color after an hour + input minutes
@@ -11,7 +11,7 @@ public class Task3 {
         String yellow = "yellow"; // 1 min
         String red = "red"; // 1 min
 
-        if(minutes % 3 > 0 && minutes % 4 > 0 && minutes % 4 < 3 && minutes % 4 >= minutes % 5) {
+        if(minutes % 3 >= 0 && minutes % 4 >= 0 && minutes % 4 < 3 && minutes % 4 >= minutes % 5) {
             System.out.println(green);
         } else if (minutes % 3 >= 0 && minutes % 2 >= 1){
             System.out.println(yellow);
